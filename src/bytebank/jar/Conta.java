c2d10e89d0090f82c83b626d7f720d107b9a9261
vitto2 <vitor2071@gmail.com>
2022-02-07 16:37:19 -0300

    subindo arquivos pendentes

---
+++ src/bytebank/jar/Conta.java
@@ -0,0 +1,15 @@
+package bytebank.jar;
+
+public class Conta {
+	public static void main(String[] args) {
+		
+		
+			class BConta { 
+				
+				double saldo; 
+				int agencia; 
+				int numero; 
+				String titular; 
+			}
+	}
+}
